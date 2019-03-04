16fdafce0b927968361bd127e98a8d8c46110da9
minhnnguyen29 <minhnnguyen29@gmail.com>
2019-03-04 12:36:09 +1100

    This piece of code contains retrieving load average, populating rrd files and create graphs.

--- graph_rrd.py
+++ graph_rrd.py
@@ -5,16 +5,16 @@ import time
 
 def graph_loadavg():
     path = "/var/sys_monitoring/loadavg_" + datetime.datetime.now().strftime('%Y-%m-%d') + ".png"
+    def1 = "DEF:load_1min=/var/sys_monitoring/loadavg_" + datetime.datetime.now().strftime('%Y-%m-%d') + ".rrd:load_1min:LAST"
+    def2 = "DEF:load_5min=/var/sys_monitoring/loadavg_" + datetime.datetime.now().strftime('%Y-%m-%d') + ".rrd:load_5min:LAST"
+    def3 = "DEF:load_15min=/var/sys_monitoring/loadavg_" + datetime.datetime.now().strftime('%Y-%m-%d') + ".rrd:load_15min:LAST"
     rrdtool.graph(path, '--imgformat', 'PNG',
                   '--width', '540',
                   '--height', '100',
                   '--start', '-1d',
                   '--end', str(int(time.time())),
                   '--vertical-label', 'ms/s',
-                  '--title', 'Load Average',
-                  'DEF:load_1min=/var/sys_monitoring/loadavg_2019-02-26.rrd:load_1min:LAST',
-                  'DEF:load_5min=/var/sys_monitoring/loadavg_2019-02-26.rrd:load_5min:LAST',
-                  'DEF:load_15min=/var/sys_monitoring/loadavg_2019-02-26.rrd:load_15min:LAST',
+                  '--title', 'Load Average', def1, def2, def3,
                   'LINE1:load_1min#0000FF:Load_1min',
                   'LINE1:load_5min#004F00:Load_5min',
                   'LINE1:load_15min#000000:Load_15min')
