a0009fa09fd2e5c7b206391870deabc6437c7023
minhnnguyen29 <minhnnguyen29@gmail.com>
2019-03-05 16:11:12 +1100

    This piece of code contains retrieving load average, populating rrd files and create graphs. Attempted to fix time issues. Changed size.

--- graph_rrd.py
+++ graph_rrd.py
@@ -9,15 +9,15 @@ def graph_loadavg():
     def2 = "DEF:load_5min=/var/sys_monitoring/loadavg_" + datetime.datetime.now().strftime('%Y-%m-%d') + ".rrd:load_5min:LAST"
     def3 = "DEF:load_15min=/var/sys_monitoring/loadavg_" + datetime.datetime.now().strftime('%Y-%m-%d') + ".rrd:load_15min:LAST"
     rrdtool.graph(path, '--imgformat', 'PNG',
-                  '--width', '540',
-                  '--height', '100',
+                  '--width', '1274',
+                  '--height', '346',
                   '--start', '-1d',
                   '--end', str(int(time.time())),
                   '--vertical-label', 'ms/s',
                   '--title', 'Load Average', def1, def2, def3,
                   'LINE1:load_1min#0000FF:Load_1min',
                   'LINE1:load_5min#004F00:Load_5min',
-                  'LINE1:load_15min#000000:Load_15min')
+                  'LINE1:load_15min#ff69b4:Load_15min')
     #print("Graph created!")
 
 def main():
