f03aeed86f8363f1db98d331ac7b3f9c4da6e15b
minhnnguyen29 <minhnnguyen29@gmail.com>
2019-03-05 16:29:39 +1100

    This piece of code contains retrieving load average, populating rrd files and create graphs. Attempted to fix time issues. Changed size of png.
    Attempted to fix changed size.
    Changed time setting in graph_rrd.py 1
    Changed time setting in generateDailyGraph.py 2
    
    Changed time setting in generateDailyGraph.py
    just changed owner to try th
    changed owner -R
    changed owner -R

--- generateDailyReport.py
+++ generateDailyReport.py
@@ -3,13 +3,14 @@ import os
 import sys
 sys.path.append('/home/minh/PycharmProjects/Uptime-like_Information/venv/lib/python3.6/site-packages')
 import datetime
+import time
 import rrdtool
 
 def createFile():
     data_sources = ['DS:load_1min:GAUGE:60:0:U', 'DS:load_5min:GAUGE:60:0:U',
                     'DS:load_15min:GAUGE:60:0:U']
     file_name = "/var/sys_monitoring/loadavg_" + datetime.datetime.now().strftime('%Y-%m-%d') + ".rrd"
-    rrdtool.create(file_name, "--start", "1551053682", "--step", "60",
+    rrdtool.create(file_name, "--start", str(int(time.time())), "--step", "60",
                    data_sources,"RRA:LAST:0.5:1:1440")
 
 def main():
